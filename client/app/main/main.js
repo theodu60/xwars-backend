efd6dd48559034591d7bb8d87851a123d3bdeee8
theodu60 <hemachandra.theophile@gmail.com>
2015-01-28 10:38:28 +0100

    Premiers commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('xwarsBackendApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
